d1535b31f5bc9a6c545eed43f44071cbe7b01872
aravindanath <20963786+aravindanath@users.noreply.github.com>
2022-11-13 17:01:13 +0530

    Adding all files

---
+++ src/test/java/day1/LaunchYoutube.java
@@ -0,0 +1,38 @@
+package day1;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class LaunchYoutube {
+
+
+    @Test
+    public void setup() throws MalformedURLException {
+
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("appActivity","com.google.android.youtube.app.honeycomb.Shell$HomeActivity");
+        desiredCapabilities.setCapability("appPackage","com.google.android.youtube");
+        desiredCapabilities.setCapability("platformName","Android");
+        desiredCapabilities.setCapability("automationName","UiAutomator2");
+        desiredCapabilities.setCapability("deviceName","emulator-5556");
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        driver.findElement(By.xpath("//android.widget.Button[@text='Allow']")).click();
+
+        driver.findElement(By.xpath("//android.widget.ImageView[@content-desc='Search']")).click();
+
+        driver.findElement(By.id("com.google.android.youtube:id/search_edit_text")).sendKeys("aadvik raj sharma swimming");
+
+
+
+    }
+
+}
